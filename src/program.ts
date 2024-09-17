a10549b06c617af731113d1ae66d14448b01e64f
Mayank Pareek <mpareek365@gmail.com>
2024-09-17 18:18:20 -0400

    Add LLM model configuration file

--- src/program.ts
+++ src/program.ts
@@ -1,7 +1,31 @@
 import { Command } from 'commander';
 import aiResponse from './ai';
+import * as fs from 'fs';
 import { processFile } from './utils/fileHandler';
 const program = new Command();
+
+//Configuration for LLM Model
+interface Config {
+  model: string;
+  temperature: string;
+}
+
+// Function to load the configuration
+const loadConfig = (filePath: string): Config => {
+  const data = fs.readFileSync(filePath, 'utf-8');
+  const configData = JSON.parse(data);
+  //validate parsed data
+  if (
+    typeof config.model !== 'string' ||
+    typeof config.temperature !== 'string'
+  ) {
+    console.error('Missing or invalid LLM configuration, check config.json');
+  }
+  return configData;
+};
+
+// Load config from the config.json file
+const config: Config = loadConfig('./config.json');
 
 program
   .name('dev-mate-cli')
@@ -9,11 +33,15 @@ program
   .version('0.1.0', '-v, --version', 'output the current version') // Set the version number and command to display it
   .option(
     '-m,--model <model-name>', // Option to specify the AI model
-    'specify the model to use',
-    'google/gemma-2-9b-it:free',
+    'specify the model to use, check available models at https://openrouter.ai/models/',
+    config.model,
   )
   .option('-o,--output <output-file>', 'file to output response') // Option to specify the output file
-  .option('-t, --temperature <temperature>', 'set the model temperature', '0.5') // Option to set the model temperature
+  .option(
+    '-t, --temperature <temperature>',
+    'set the model temperature',
+    config.temperature,
+  ) // Option to set the model temperature
   .option('-u, --token-usage', 'output token usage data') // Option to display token usage data
   .argument('<files...>') // Define the required file argument
   .action(async function (files: string[]) {
